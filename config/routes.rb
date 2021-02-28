724819ddab8c9ca611db28b4b769ed0d38a8e3ee
noharahitomi <69335777+noharahitomi@users.noreply.github.com>
2021-02-28 12:16:10 +0900

    既読機能のサーバーサイド実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'posts#index' # postsというパスに、GETメソッドでリクエストが送られたときに、postsコントローラーのindexアクションを呼び出すという設定
   post 'posts', to: 'posts#create' # postsというパスに、POSTメソッドでリクエストが送られたときに、postsコントローラーのcreateアクションを呼び出す設定
+  get 'posts/:id', to: 'posts#checked'
 end
