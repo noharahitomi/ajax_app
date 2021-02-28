39451ddcf2c36b5e1855eb187cd95a80c3e683f7
noharahitomi <69335777+noharahitomi@users.noreply.github.com>
2021-02-28 11:56:01 +0900

    AjaxAppの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index' # postsというパスに、GETメソッドでリクエストが送られたときに、postsコントローラーのindexアクションを呼び出すという設定
-  get 'posts/new', to: 'posts#new' # posts/newというパスに、GETメソッドでリクエストが送られたときに、postsコントロールのnewアクションを呼び出すという設定
+  root to: 'posts#index' # postsというパスに、GETメソッドでリクエストが送られたときに、postsコントローラーのindexアクションを呼び出すという設定
   post 'posts', to: 'posts#create' # postsというパスに、POSTメソッドでリクエストが送られたときに、postsコントローラーのcreateアクションを呼び出す設定
 end
